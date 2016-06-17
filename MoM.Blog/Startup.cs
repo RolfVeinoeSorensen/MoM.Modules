5f182c24d177bba1e00dd3b16f8ad04ab01b52c1
Rolf Veinø Sørensen <rolf@veinoe.dk>
2016-06-17 09:36:45 +0200

    refactor sitesettings to dto

--- MoM.Blog/Startup.cs
+++ MoM.Blog/Startup.cs
@@ -5,17 +5,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using MoM.Module.Models;
-using MoM.Module.Config;
+using MoM.Module.Dtos;
 
 namespace MoM.Blog
 {
     public class Startup
     {
         public IConfigurationRoot Configuration { get; set; }
-        IOptions<SiteSetting> SiteSetting;
+        IOptions<SiteSettingDto> SiteSetting;
 
-        public Startup(IHostingEnvironment env, IOptions<SiteSetting> siteSetting)
+        public Startup(IHostingEnvironment env, IOptions<SiteSettingDto> siteSetting)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.WebRootPath)
